7eea00e983a8f0640f278706f84dd1ef35f5eb34
Jordyn Jones <131826425+jadewink@users.noreply.github.com>
2025-03-28 00:05:13 -0700

    Task 2 complete - test passing

--- assignment3/assignment3.py
+++ assignment3/assignment3.py
@@ -23,3 +23,18 @@ task1_older['Age'] += 1
 
 # Save the DataFrame as a CSV file:
 task1_older.to_csv("employees.csv", index=False)
+
+# Task 2: Loading Data from CSV and JSON
+# Read data from a CSV file
+task2_employees = pd.read_csv('employees.csv')
+# print(task2_employees)
+
+# Read data from a JSON file:
+json_employees = pd.read_json('additional_employees.json')
+# print(json_employees)
+
+#Combine DataFrames
+# Combine the data from the JSON file into the DataFrame Loaded from the CSV file and save it in the variable more_employees.
+# Print the combined Dataframe and run the tests.
+more_employees = pd.concat([task2_employees, json_employees], ignore_index=True)
+# print(more_employees)
