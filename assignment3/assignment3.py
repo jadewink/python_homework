40921842918e7008dbc44f9fd3ef4a76d84f4563
Jordyn Jones <131826425+jadewink@users.noreply.github.com>
2025-03-29 20:18:16 -0700

    Task 4 Complete - test passing

--- assignment3/assignment3.py
+++ assignment3/assignment3.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 #Task 1: Introduction to Pandas - Creating and Manipulating DataFrames
 #Create dataframe from dictionary
@@ -42,14 +43,60 @@ more_employees = pd.concat([task2_employees, json_employees], ignore_index=True)
 # Task 3: Data Inspection - Using Head, Tail, and Info Methods
 # Use the head() method:
 first_three = more_employees.head(3)
-print(first_three)
+# print(first_three)
 
 # Use the tail() method:
 last_two = more_employees.tail(2)
-print(last_two)
+# print(last_two)
 
 # Get the shape of a DataFrame
 employee_shape = more_employees.shape
 
 # Use the info() method:
-print(more_employees.info())
+# print(more_employees.info())
+
+# Task #4
+# Create a DataFrame from dirty_data.csv file and assign it to the variable dirty_data.
+dirty_data = pd.read_csv('dirty_data.csv')
+# print(dirty_data)
+
+# Create a copy of the dirty data in the variable clean_data (use the copy() method). You will use data cleaning methods to update clean_data.
+clean_data = dirty_data.copy()
+
+# Remove any duplicate rows from the DataFrame
+clean_data = clean_data.drop_duplicates()
+
+# Convert Age to numeric and handle missing values
+clean_data["Age"] = pd.to_numeric(clean_data["Age"], errors="coerce")
+clean_data["Age"] = clean_data["Age"].fillna(1)
+clean_data["Age"] = clean_data["Age"].astype(int)
+# print(clean_data)
+
+# Convert Salary to numeric and replace known placeholders (unknown, n/a) with NaN
+clean_data["Salary"] = pd.to_numeric(clean_data["Salary"], errors="coerce")
+clean_data["Salary"] = clean_data["Salary"].replace("n/a", pd.NA)
+clean_data["Salary"] = clean_data["Salary"].replace("unknown", pd.NA)
+# clean_data["Salary"] = clean_data["Salary"].astype(int)
+
+
+# Fill missing numeric values (use fillna).  Fill Age with the mean and Salary with the median
+mean_age = clean_data["Age"].mean()  # ignoring NaNs
+# clean_data["Age"] = clean_data["Age"].fillna(mean_age)
+clean_data["Age"] = clean_data["Age"].replace(1, mean_age)
+clean_data["Age"] = clean_data["Age"].astype(int)
+
+median_salary = clean_data["Salary"].median()
+clean_data["Salary"] = clean_data["Salary"].fillna(median_salary)
+clean_data["Salary"] = clean_data["Salary"].astype(int)
+print(clean_data)
+
+# Convert Hire Date to datetime
+clean_data["Hire Date"] = pd.to_datetime(clean_data["Hire Date"], errors="coerce")
+print(clean_data)
+
+# Strip extra whitespace and standardize Name and Department as uppercase
+clean_data["Name"] = clean_data["Name"].str.strip()
+clean_data["Department"] = clean_data["Department"].str.strip()
+clean_data["Name"] = clean_data["Name"].str.upper()
+clean_data["Department"] = clean_data["Department"].str.upper()
+
