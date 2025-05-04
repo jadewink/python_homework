e915a0b3f16e14a960e340124416e2124496623c
Jordyn Jones <131826425+jadewink@users.noreply.github.com>
2025-05-03 17:59:18 -0700

    Task 2 Completed

--- assignment8/advanced_sql.py
+++ assignment8/advanced_sql.py
@@ -27,3 +27,40 @@ results = cursor.fetchall()
 print(results)
 
 conn.close()
+
+# Task 2
+# For each customer, find the average price of their orders.  This can be done with a subquery. You compute the price of each order as in part 1, 
+# but you return the customer_id and the total_price.  That's the subquery. You need to return the total price using AS total_price, and you need 
+# to return the customer_id with AS customer_id_b, for reasons that will be clear in a moment.  In your main statement, you left join the customer 
+# table with the results of the subquery, using ON customer_id = customer_id_b.  You aliased the customer_id column in the subquery so that the 
+# column names wouldn't collide.  Then group by customer_id -- this GROUP BY comes after the subquery -- and get the average of the total price of 
+# the customer orders.  Return the customer name and the average_total_price.
+
+import sqlite3
+
+# Connect to the database
+conn = sqlite3.connect("../db/lesson.db")
+cursor = conn.cursor()
+
+# Aggregation with HAVING
+query = """
+SELECT customer_name, ROUND(AVG(total_price),2) as average_total_price
+FROM customers c
+LEFT JOIN (
+    SELECT o.customer_id AS customer_id_b, sum(p.price * li.quantity) AS total_price
+    FROM orders AS o 
+    JOIN line_items AS li ON o.order_id = li.order_id
+    JOIN products AS p on p.product_id = li.product_id
+    GROUP BY o.order_id
+    ORDER BY o.order_id
+) AS total_price
+ON c.customer_id = customer_id_b
+GROUP BY c.customer_id
+"""
+
+# Execute and fetch results
+cursor.execute(query)
+results = cursor.fetchall()
+print(results)
+
+conn.close()
